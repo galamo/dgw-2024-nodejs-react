1409a3d2010c1a905cc7240c32ac13358f27e41e
galamo <galamouyal88@gmail.com>
2024-07-02 11:21:18 +0300

    a

--- api/dist/index.js
+++ api/dist/index.js
@@ -1,5 +1,12 @@
 "use strict";
-setTimeout(() => {
-    console.log("Node js hello world!!");
-}, 200);
-function init() { }
+var CATEGORIES;
+(function (CATEGORIES) {
+    CATEGORIES[CATEGORIES["MEAT"] = 1] = "MEAT";
+    CATEGORIES[CATEGORIES["DAIRY"] = 2] = "DAIRY";
+    CATEGORIES[CATEGORIES["DRINKS"] = 3] = "DRINKS";
+})(CATEGORIES || (CATEGORIES = {}));
+const p1 = { name: "coffee", price: 10, category: CATEGORIES.DRINKS };
+const p2 = { name: "coffee", price: 10, category: CATEGORIES.DRINKS };
+function getUser() {
+    return { user: "galamouyal88@gmail.com" };
+}
--- api/src/index.ts
+++ api/src/index.ts
@@ -1,5 +1,24 @@
-setTimeout(() => {
-    console.log("Node js hello world!!")
-}, 200);
+interface IUser {
+    user: string
+}
 
-function init() { }
+type Product = {
+    name: string,
+    price: number,
+    category: CATEGORIES
+}
+
+enum CATEGORIES {
+    MEAT = 1,
+    DAIRY = 2,
+    DRINKS = 3
+}
+
+const p1: Product = { name: "coffee", price: 10, category: CATEGORIES.DRINKS }
+const p2: Product = { name: "coffee", price: 10, category: CATEGORIES.DRINKS }
+
+
+
+function getUser(): IUser {
+    return { user: "galamouyal88@gmail.com" }
+}
